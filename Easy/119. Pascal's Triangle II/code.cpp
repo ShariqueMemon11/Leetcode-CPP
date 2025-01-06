0947a1eb76d5def61812d8c76fb727c2870108b3
Shariq.x <113132879+ShariqueMemon11@users.noreply.github.com>
2025-01-06 23:44:02 +0500

    Pascal's Triangle II

---
+++ Easy/119. Pascal's Triangle II/code.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+        vector<vector <int>> rows(rowIndex);
+        for(int i = 0 ; i < rowIndex;i++){
+            rows[i].resize(i+1);
+            rows[i][0]=1;
+            rows[i][i]=1;
+            for(int j = 1; j < i; j++){
+                rows[i][j]=rows[i-1][j-1]+rows[i-1][j];
+            }
+        }
+        return rows[rowIndex-1];
+    }
+};
