9429deed2524d42d6efdcdc6460a9037582b8333
Shariq.x <113132879+ShariqueMemon11@users.noreply.github.com>
2025-01-08 00:02:10 +0500

    Update code.cpp

--- Easy/136. Single Number/code.cpp
+++ Easy/136. Single Number/code.cpp
@@ -1,1 +1,18 @@
-
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        unordered_map<int, int> mymap;
+        for(int i = 0 ; i < size(nums) ; i++){
+            if(mymap.find(nums[i])!=mymap.end()){
+                mymap.erase(nums[i]);
+            }
+            else{
+                mymap[nums[i]]=i;
+            }
+        }
+        for(auto it : mymap){
+            return it.first;
+        }
+        return {};
+    }
+};
